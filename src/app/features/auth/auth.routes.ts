294c70d7774b629e1491702711467f41cbb91780
fodlanstrash <112221568+fodlanstrash@users.noreply.github.com>
2025-11-21 17:50:15 -0500

    Desarrollo inicial de integracion backend funcion registro

---
+++ src/app/features/auth/auth.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+
+export const AUTH_ROUTES: Routes = [
+	{
+		path: 'iniciar-sesion',
+		loadComponent: () => import('./pages/iniciar-sesion/iniciar-sesion.component').then(m => m.IniciarSesionComponent)
+	},
+	{
+		path: 'recuperar-contrasena',
+		loadComponent: () => import('./pages/recuperar-contrasena/recuperar-contrasena.component').then(m => m.RecuperarContrasenaComponent)
+	},
+	{
+		path: 'registro',
+		loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent)
+	},
+	{ path: '', redirectTo: 'iniciar-sesion', pathMatch: 'full' }
+];
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://impulso-fit-api.onrender.com/api/v1'
+};
