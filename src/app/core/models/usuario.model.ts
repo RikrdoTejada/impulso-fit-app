414e99a06737324caffab025c912e6ace35a7581
Ricardo Tejada <ricardo.martin2003@gmail.com>
2025-11-21 19:44:45 -0500

    feat. authservice - login

---
+++ src/app/core/models/usuario.model.ts
@@ -0,0 +1,34 @@
+export enum RoleType {
+    ROLE_USER = 'ROLE_USER',
+    ROLE_ADMIN = 'ROLE_ADMIN'
+}
+
+//request
+
+export interface RegisterRequestDTO {
+  email: string;
+  password: string;
+}
+
+export interface LoginRequestDTO {
+  email: string;
+  password: string;
+}
+
+//response
+
+export interface PerfilResponseDTO {
+    id: string;
+    email: string;
+    names: string;
+    role: RoleType;
+    bloqueado: boolean;
+}
+
+export interface AuthResponseDTO {
+    token: string;
+    type: string;
+    email: string;
+    name: string;
+    role?: RoleType;
+}
